8d0a451cce926accca17d42add5a71c31d7b1c83
Daniel Stansberry <danielsstansberry@gmail.com>
2018-10-29 23:00:42 +0000

    Updates

--- media.js
+++ media.js
@@ -3,20 +3,34 @@
     "albumName": "Random Access Memories",
     "albumArtwork": "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Random_Access_Memories.jpg/220px-Random_Access_Memories.jpg",
     "songs": [
-      "/static/Give%20life%20Back%20to%20Music.mp3",
-      "/static/The%20Game%20Of%20Love.mp3",
-      "/static/Giorgio%20by%20Moroder.mp3",
-      "/static/Within.mp3",
-      "/static/Instant Crush.mp3",
-      "/static/Lose Yourself To Dance.mp3",
-      "/static/Touch (feat. Paul Williams).mp3",
-      "/static/Get Lucky (feat. Pharrell Williams).mp3",
-      "/static/Beyond.mp3",
-      "/static/Motherboard.mp3",
-      "/static/Fragments of Time (feat. Todd Edwards).mp3",
-      "/static/Doin\" It Right (feat. Panda Bear).mp3",
-      "/static/Contact.mp3",
-      "/static/Horizon.mp3"
-    ]
+      "static/Random Access Memories/Give%20Life%20Back%20to%20Music.mp3",
+      "static/Random Access Memories/The%20Game%20of%20Love.mp3",
+      "static/Random Access Memories/Giorgio%20by%20Moroder.mp3",
+      "static/Random Access Memories/Within.mp3",
+      "static/Random Access Memories/Instant Crush.mp3",
+      "static/Random Access Memories/Lose Yourself To Dance.mp3",
+      "static/Random Access Memories/Touch (feat. Paul Williams).mp3",
+      "static/Random Access Memories/Get Lucky (feat. Pharrell Williams).mp3",
+      "static/Random Access Memories/Beyond.mp3",
+      "static/Random Access Memories/Motherboard.mp3",
+      "static/Random Access Memories/Fragments of Time (feat. Todd Edwards).mp3",
+      "static/Random Access Memories/Doin\\' It Right (feat. Panda Bear).mp3",
+      "static/Random Access Memories/Contact.mp3",
+      "static/Random Access Memories/Horizon.mp3"]
+  },
+  {
+    "albumName": "Random Access Memories",
+    "albumArtwork": "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Random_Access_Memories.jpg/220px-Random_Access_Memories.jpg",
+    "songs": [
+      "static/The%20Game%20of%20Love.mp3",
+      "static/Giorgio%20by%20Moroder.mp3",
+      "static/Instant Crush.mp3",
+      "static/Get Lucky (feat. Pharrell Williams).mp3",
+      "static/Beyond.mp3",
+      "static/Motherboard.mp3",
+      "static/Fragments of Time (feat. Todd Edwards).mp3",
+      "static/Doin\\' It Right (feat. Panda Bear).mp3",
+      "static/Contact.mp3",
+      "static/Horizon.mp3"]
   }
 ]
