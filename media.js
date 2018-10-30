5d7ea5852eeea2e7d05b555a711c97cba8f10760
Daniel Stansberry <danielsstansberry@gmail.com>
2018-10-30 19:54:51 +0000

    Updates

--- media.js
+++ media.js
@@ -1,7 +1,7 @@
 [
   {
     "albumName": "Random Access Memories",
-    "albumArtwork": "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Random_Access_Memories.jpg/220px-Random_Access_Memories.jpg",
+    "albumArtwork": "static/Random Access Memories/Random Access Memories.jpg",
     "songs": [
       "static/Random Access Memories/Give%20Life%20Back%20to%20Music.mp3",
       "static/Random Access Memories/The%20Game%20of%20Love.mp3",
@@ -14,23 +14,55 @@
       "static/Random Access Memories/Beyond.mp3",
       "static/Random Access Memories/Motherboard.mp3",
       "static/Random Access Memories/Fragments of Time (feat. Todd Edwards).mp3",
-      "static/Random Access Memories/Doin\\' It Right (feat. Panda Bear).mp3",
+      "static/Random Access Memories/Doin' It Right (feat. Panda Bear).mp3",
       "static/Random Access Memories/Contact.mp3",
-      "static/Random Access Memories/Horizon.mp3"]
+      "static/Random Access Memories/Horizon.mp3"],
+    "songNames": [
+      "Give Life Back to Music",
+      "The Game of Love",
+      "Giorgio by Moroder",
+      "Within",
+      "Instant Crush",
+      "Lose Yourself To Dance",
+      "Touch (feat. Paul Williams)",
+      "Get Lucky (feat. Pharrell Williams)",
+      "Beyond",
+      "Motherboard",
+      "Fragments of Time (feat. Todd Edwards)",
+      "Doin' It Right (feat. Panda Bear)",
+      "Contact",
+      "Horizon"
+    ]
   },
   {
-    "albumName": "Random Access Memories",
-    "albumArtwork": "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Random_Access_Memories.jpg/220px-Random_Access_Memories.jpg",
+    "albumName": "Pharmacy",
+    "albumArtwork": "static/Pharmacy/Pharmacy.jpg",
     "songs": [
-      "static/The%20Game%20of%20Love.mp3",
-      "static/Giorgio%20by%20Moroder.mp3",
-      "static/Instant Crush.mp3",
-      "static/Get Lucky (feat. Pharrell Williams).mp3",
-      "static/Beyond.mp3",
-      "static/Motherboard.mp3",
-      "static/Fragments of Time (feat. Todd Edwards).mp3",
-      "static/Doin\\' It Right (feat. Panda Bear).mp3",
-      "static/Contact.mp3",
-      "static/Horizon.mp3"]
+      "static/Pharmacy/Forever Tonight.mp3",
+      "static/Pharmacy/Gold Dust.mp3",
+      "static/Pharmacy/In My Head.mp3",
+      "static/Pharmacy/Runaway (U & I).mp3",
+      "static/Pharmacy/Dancin' To the Sound of a Broken Heart.mp3",
+      "static/Pharmacy/Louder, Harder, Better.mp3",
+      "static/Pharmacy/Call If You Need Me.mp3",
+      "static/Pharmacy/Peanut Butter Jelly.mp3",
+      "static/Pharmacy/Firebird.mp3",
+      "static/Pharmacy/Don't care.mp3",
+      "static/Pharmacy/You.mp3",
+      "static/Pharmacy/Water.mp3"],
+      "songNames": [
+        "Forever Tonight",
+        "Gold Dust",
+        "In My Head",
+        "Runaway (U & I)",
+        "Dancin To the Sound of a Broken Heart",
+        "Louder, Harder, Better",
+        "Call If You Need Me",
+        "Peanut Butter Jelly",
+        "Firebird",
+        "Don't Care",
+        "You",
+        "Water"
+      ]
   }
 ]
