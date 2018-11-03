e5926f170c80f7f052f098c0a0d7a9a5950b2788
Daniel Stansberry <danielsstansberry@gmail.com>
2018-11-03 03:30:48 +0000

    update to version 1.0.1

--- example/src/components/Main.js
+++ example/src/components/Main.js
@@ -41,7 +41,7 @@ export default class Main extends Component {
               playlistIsPlaying={this.props.playlistIsPlaying} />}
         </div>
         <div>
-          Documentation goes here
+          Documentation goes here (Version 1.0.1)
         </div>
       </main>
     )
