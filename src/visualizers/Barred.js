89b59fbbc3ca5c34a4bedba1ea39be7d14fc7380
Daniel <stansberry2295@gmail.com>
2018-11-08 23:45:11 -0800

    lint and clean some code, working on new visualizer and responsive design next

--- src/visualizers/Barred.js
+++ src/visualizers/Barred.js
@@ -1,22 +1,20 @@
 import * as THREE from 'three'
 import { Spectrum } from './utils/Spectrum'
 
-
 let fsize = 4096
 let numBars = 64
 let vertexShader = [
-  "void main() {",
-  "gl_Position = gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-  "}"
-].join('\n');
+  'void main() {',
+  'gl_Position = gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+  '}'
+].join('\n')
 let fragmentShader = [
-  "void main() {",
-  "gl_FragColor = vec4( gl_FragCoord.y/500.0, 0, gl_FragCoord.y/1000.0, 1.0 );",
-  "}"
-].join('\n');
+  'void main() {',
+  'gl_FragColor = vec4( gl_FragCoord.y/500.0, 0, gl_FragCoord.y/1000.0, 1.0 );',
+  '}'
+].join('\n')
 
 let plane, group
-
 
 class Barred {
   constructor(scene, camera, renderer, canvasRef, analyser) {
@@ -30,41 +28,35 @@ class Barred {
   init() {
     // set up scene environment
     this.renderer.clear()
-    this.camera = new THREE.OrthographicCamera( this.canvasRef.current.clientWidth / - 2, this.canvasRef.current.clientWidth / 2, this.canvasRef.current.clientHeight / 2, this.canvasRef.current.clientHeight / - 2, 1, 1000 );
-    this.camera.position.y = 150;
-    this.camera.position.z = 500;
+    this.camera = new THREE.OrthographicCamera(this.canvasRef.current.clientWidth / -2, this.canvasRef.current.clientWidth / 2, this.canvasRef.current.clientHeight / 2, this.canvasRef.current.clientHeight / -2, 1, 1000)
+    this.camera.position.y = 150
+    this.camera.position.z = 500
     this.renderer.setClearColor('#000000')
 
-    //copy paste
-
-    group = new THREE.Object3D();
-    this.spectrum = new Spectrum();
-    this.analyser.fftSize = fsize;
-    let bufferLength = this.analyser.frequencyBinCount;
-    this.dataArray = new Uint8Array(bufferLength);
+    group = new THREE.Object3D()
+    this.spectrum = new Spectrum()
+    this.analyser.fftSize = fsize
+    let bufferLength = this.analyser.frequencyBinCount
+    this.dataArray = new Uint8Array(bufferLength)
 
     // view.useOrthographicCamera();
 
-    let positionX = -20 * (numBars / 2);
+    let positionX = -20 * (numBars / 2)
 
     for (var i = 0; i < numBars; i++) {
-
-      let geometry = new THREE.PlaneBufferGeometry(18, 10, 1);
-      var uniforms = {};
+      let geometry = new THREE.PlaneBufferGeometry(18, 10, 1)
+      var uniforms = {}
       var material = new THREE.ShaderMaterial({
         uniforms: uniforms,
         vertexShader: vertexShader,
         fragmentShader: fragmentShader
-      });
-      plane = new THREE.Mesh(geometry, material);
-      plane.position.x = positionX;
-      positionX += 20;
-
-      group.add(plane);
+      })
+      plane = new THREE.Mesh(geometry, material)
+      plane.position.x = positionX
+      positionX += 20
+      group.add(plane)
     }
-
-    this.scene.add(group);
-    // end copy paste
+    this.scene.add(group)
 
     this.canvasRef.current.appendChild(this.renderer.domElement)
     this.start()
@@ -88,13 +80,13 @@ class Barred {
   }
 
   renderScene() {
-    this.analyser.getByteFrequencyData(this.dataArray);
-    let visualArray = this.spectrum.GetVisualBins(this.dataArray, numBars, 4, 1300);
+    this.analyser.getByteFrequencyData(this.dataArray)
+    let visualArray = this.spectrum.GetVisualBins(this.dataArray, numBars, 4, 1300)
     if (group) {
       for (var i = 0; i < visualArray.length; i++) {
-        group.children[i].geometry.attributes.position.array[1] = visualArray[i];
-        group.children[i].geometry.attributes.position.array[4] = visualArray[i];
-        group.children[i].geometry.attributes.position.needsUpdate = true;
+        group.children[i].geometry.attributes.position.array[1] = visualArray[i]
+        group.children[i].geometry.attributes.position.array[4] = visualArray[i]
+        group.children[i].geometry.attributes.position.needsUpdate = true
       }
     }
     this.renderer.render(this.scene, this.camera)
--- src/visualizers/HillFog.js
+++ src/visualizers/HillFog.js
@@ -1,15 +1,15 @@
 import * as THREE from 'three'
 
 var vertexShader = [
-  "void main() {",
-      "gl_Position = gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-  "}"
-].join('\n');
+  'void main() {',
+  'gl_Position = gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+  '}'
+].join('\n')
 var fragmentShader = [
-  "void main() {",
-      "gl_FragColor = vec4( gl_FragCoord.y/500.0, 0, gl_FragCoord.y/1000.0, 1.0 );",
-  "}"
-].join('\n');
+  'void main() {',
+  'gl_FragColor = vec4( gl_FragCoord.y/500.0, 0, gl_FragCoord.y/1000.0, 1.0 );',
+  '}'
+].join('\n')
 
 class HillFog {
   constructor(scene, camera, renderer, canvasRef, analyser) {
@@ -23,20 +23,19 @@ class HillFog {
   init() {
     // set up scene environment
     this.renderer.setClearColor('#000000')
-    this.camera = new THREE.OrthographicCamera( this.canvasRef.current.clientWidth / - 2, this.canvasRef.current.clientWidth / 2, this.canvasRef.current.clientHeight / 2, this.canvasRef.current.clientHeight / - 2, 1, 1000 );
-    this.camera.position.y = 150;
-    this.camera.position.z = 500;
+    this.camera = new THREE.OrthographicCamera(this.canvasRef.current.clientWidth / -2, this.canvasRef.current.clientWidth / 2, this.canvasRef.current.clientHeight / 2, this.canvasRef.current.clientHeight / -2, 1, 1000)
+    this.camera.position.y = 150
+    this.camera.position.z = 500
     // set up your array objects here
     this.geometry = new THREE.PlaneBufferGeometry(900, 40, 127)
-    var uniforms = {};
+    var uniforms = {}
     var material = new THREE.ShaderMaterial({
       uniforms: uniforms,
       vertexShader: vertexShader,
       fragmentShader: fragmentShader
-    });
-    let plane = new THREE.Mesh(this.geometry, material);
-    this.scene.add(plane);
-
+    })
+    let plane = new THREE.Mesh(this.geometry, material)
+    this.scene.add(plane)
 
     this.canvasRef.current.appendChild(this.renderer.domElement)
     this.start()
@@ -67,15 +66,15 @@ class HillFog {
   }
 
   renderScene() {
-    this.analyser.fftSize = 256;
-    var bufferLength = this.analyser.frequencyBinCount;
-    var dataArray = new Uint8Array(bufferLength);
-    this.analyser.getByteTimeDomainData(dataArray);
+    this.analyser.fftSize = 256
+    var bufferLength = this.analyser.frequencyBinCount
+    var dataArray = new Uint8Array(bufferLength)
+    this.analyser.getByteTimeDomainData(dataArray)
     if (this.geometry) {
       for (var i = 0; i < bufferLength; i++) {
-        this.geometry.attributes.position.array[i * 3 + 1] = dataArray[i] / 3;
+        this.geometry.attributes.position.array[i * 3 + 1] = dataArray[i] / 3
       }
-      this.geometry.attributes.position.needsUpdate = true;
+      this.geometry.attributes.position.needsUpdate = true
     }
 
     this.renderer.render(this.scene, this.camera)
